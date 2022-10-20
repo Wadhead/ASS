8b33f68e0e75f491d76cfe49eec2342922ef77d4
Vladimir <bvckolomna1307@gmail.com>
2022-10-20 11:45:57 +0300

    Movies

--- porojekt.js
+++ porojekt.js
@@ -1,3 +1,21 @@
 "use strict";
- alert("Привет");
- console.log('love');
+const numberOFfilms= +prompt ("Сколько фильмов вы уже посмотрели",'' );
+
+ const personalMovieDB = {
+    cont: numberOFfilms,
+    movies:{},
+    actors: {},
+    genres:[],
+    privat: false
+
+ };
+const 
+    a = prompt ('Один из последних просмотренных фильмов', ''),
+    b = prompt ('На сколько оценете его?', ''),
+    c = prompt ('Один из последних просмотренных фильмов', ''),
+    d = prompt ('На сколько оценете его?', '');
+
+ personalMovieDB.movies[a] = b;
+ personalMovieDB.movies[c] = d;
+
+ console.log(personalMovieDB);
